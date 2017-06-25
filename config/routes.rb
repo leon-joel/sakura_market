4b7fa1bd804d87bfcf87f485d24b2ab02919488c
L.Joel <leopard10000@gmail.com>
2017-06-25 23:18:58 +0900

    管理者用のRoutingを追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,16 @@ Rails.application.routes.draw do
   resources :cart_products, only: [ :index, :create, :update, :destroy ]
   resources :orders
 
+  namespace :admin do
+    root "sessions#new"
+    get 'login' => 'sessions#new', as: :login
+    resource :session, only: [ :create, :destroy ]
+
+    resources :users, only: [ :index, :show, :edit, :update, :destroy ]
+    resources :products
+    resources :orders, only: [ :index, :show, :edit, :update ]
+
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
