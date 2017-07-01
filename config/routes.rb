7ec3aecd92c5141ae2e687d8492e67f14a48ecce
L.Joel <leopard10000@gmail.com>
2017-07-01 14:46:10 +0900

    サインイン: Controller/Viewを実装中

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   root "products#index"
 
-  resource :account, { controller: :users }
+  resource :account, { controller: :users, as: :user }
 
   get 'login' => 'sessions#new', as: :login
   resource :session, only: [ :create, :destroy ]
@@ -13,14 +12,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "sessions#new"
+
     get 'login' => 'sessions#new', as: :login
     resource :session, only: [ :create, :destroy ]
 
     resources :users, only: [ :index, :show, :edit, :update, :destroy ]
     resources :products
     resources :orders, only: [ :index, :show, :edit, :update ]
-
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
