65019bb77e869a5c8ed164645fbb35666949169a
L.Joel <leopard10000@gmail.com>
2017-07-02 00:11:47 +0900

    ログイン: View+Controller実装 ※session維持の仕組みは未実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      redirect_to root_path
+    else
+      # TODO: エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
   end
 
   def destroy
   end
+
+  private
+
 end
