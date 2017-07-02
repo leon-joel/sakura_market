eef36a11f50880ebf232077db12766f27583c3e8
L.Joel <leopard10000@gmail.com>
2017-07-02 11:35:31 +0900

    ユーザー登録情報編集: とりあえず編集は出来るようになった
    TODO: emailはUNIQUE制約が必要
    TODO: password変更できるように
    TODO: email変更時には認証が必要 ※そもそもサインイン時にもやってない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
-  # GET /users/new
+  # サインイン
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+  # ユーザー登録情報の編集
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -27,8 +23,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -41,8 +35,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
+  # 退会
   def destroy
     @user.destroy
     respond_to do |format|
@@ -52,9 +45,11 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = current_user
+      if @user.nil?
+        redirect_to :root
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
