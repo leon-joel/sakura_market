caab96b04bceec16b6c92453cf31e7f8878b8af4
Leon <leopard10000@gmail.com>
2017-10-05 23:04:56 +0900

    商品マスター: 商品の追加

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -18,15 +18,10 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.save
+      redirect_to :admin_products, notice: 'Product was successfully created.'
+    else
+      render "admin/products/new"
     end
   end
 
@@ -34,7 +29,7 @@ class Admin::ProductsController < ApplicationController
     if @product.update(product_params)
       redirect_to :admin_products, notice: 'Product was successfully updated.'
     else
-      render :edit
+      render "admin/products/edit"
     end
   end
 
